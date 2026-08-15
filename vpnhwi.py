24314597d5228d7b96ca6c0730bbeb8ecfb55d97
Isaías Hernández Ortiz <isaiaskisses@gmail.com>
2026-08-15 17:20:26 -0600

    Actualizar vpnhwi.py
    seguridad

--- vpnhwi.py
+++ vpnhwi.py
@@ -1,1 +1,9 @@
-pidepasboar
+import socket, threading, getpass
+
+HOST = "127.0.0.1" # Solo tu celular
+PORT = 8888
+USER = "pantera"
+PASS = getpass.getpass("Pon tu password segura: ")
+
+print(f"Proxy SEGURO en {HOST}:{PORT} user:{USER}")
+# ... resto de tu codigo ...
